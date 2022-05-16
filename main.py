341d869429d23c77d18a85f739feb00f173c50e1
Bodin <bodintiago@gmail.com>
2022-05-16 03:00:19 +0200

    add connection to db

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 import fastapi
+
+from sqlalchemy import create_engine
+
+import os
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+
+engine = create_engine("sqlite:///"+os.path.join(BASE_DIR, "database.db"), echo=True, future=True)
+
+
+# Read table
+
+# Create row
+
+# Update row
+
+# Delete row
